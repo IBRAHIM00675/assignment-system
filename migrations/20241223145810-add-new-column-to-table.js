33fa0de061fa28b89b14f5eb479e12affc342eca
ibrahim musa <miharbiibra675@gmail.com>
2024-12-24 21:22:57 +0300

    migrations/

---
+++ migrations/20241223145810-add-new-column-to-table.js
@@ -0,0 +1,14 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'role', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'user'  // Default role is 'user'
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'role');
+  }
+};
+
